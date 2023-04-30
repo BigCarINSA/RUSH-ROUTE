6373316286262cc4652bf3aadcf726a09a91acb8
Son Ngo <sonngotaiinsa@gmail.com>
2023-04-30 16:32:56 +0200

    Created Holes

--- Thanh.py
+++ Thanh.py
@@ -7,64 +7,66 @@ Created on Tue Apr  4 16:23:55 2023
 from random import randint
 import csv
 import math 
+import algo_plus_court_chemin
 
-prob_niveau={"Facile":[50,30],
-               "Moyen":[50,30],
-               "Difficile":[50,30]}
+TAILLE_MATRICE = { "facile"    : (10, 20),
+                   "moyen"      : (15, 30),
+                   "difficile"  : (20, 40) } 
+
+CARACTERE_CELL = { "trou"     : 0, #Ne pas passer
+                   "sol plat" : 1, #Vitesse normal
+                   "pente"    : 2, #Vitesse / 2
+                   }
+
+POURCENT_TROU_INTERVALLE = (20, 50) #20 à 50 %
 
 class Matrice():
-    def __init__(self, niveau, maze):
+    def __init__(self, niveau):
         #Initiliser le matrice
-        self.matrice =[]
         self.niveau = niveau
+        (self.width, self.height) = TAILLE_MATRICE[ self.niveau ] 
         self.créer_matrice_brut()
         
-        self.bloc = maze[0]
-        self.carrée_plus_vite=maze[1]
-        self.debut = (0,0)
-        self.chemin = []
-        self.distance = 0
-        
         self.posit_début_fin()
-        self.créer_matrice_net()
-        self.créer_file_csv()
+        self.superficie = self.width * self.height
+        self.pourcent_de_trou = randint(POURCENT_TROU_INTERVALLE[0], POURCENT_TROU_INTERVALLE[1])
+        self.nb_trous = self.pourcent_de_trou * self.superficie // 100
+        
+        self.algo = algo_plus_court_chemin(self.matrice)
+        self.creer_des_trous()
+        self.algo.print_map(self.matrice)
+        print(self.algo.way)
         
     def créer_matrice_brut(self):
-        if self.niveau == "Facile":
-            for y in range(3):
-                ligne = []
-                for x in range(3):
-                    ligne.append(3)
-                self.matrice.append(ligne)
-                
-        elif self.niveau == "Normal":
-            for y in range(15):
-                ligne = []
-                for x in range(30):
-                    ligne.append(3)
-                self.matrice.append(ligne)
-                
-        else:
-            for y in range(20):
-                ligne = []
-                for x in range(40):
-                    ligne.append(3)
-                self.matrice.append(ligne)
+        self.matrice = []
+        for i in range(self.width):
+            row = []
+            for j in range(self.height):
+                row.append(1)
+            self.matrice.append(row.copy())                
         
     def posit_début_fin(self):
-        self.matrice[0][0] = -1
-        self.matrice[len(self.matrice)-1][len(self.matrice[0])-1] = -2   
+        self.debut = (randint(0, self.width), randint(0, self.width))
+        self.fin = (randint(0, self.width), randint(0, self.width))
+        while self.fin == self.debut:  
+            self.fin = (randint(0, self.width), randint(0, self.width))
+        
+        self.matrice[ self.debut[0] ][ self.debut[1] ] = -1
+        self.matrice[ self.fin[0] ][ self.fin[1] ] = -2   
 
-    def créer_matrice_net(self):
-        prob_0 = prob_niveau[self.niveau][0]
-        prob_1 = prob_niveau[self.niveau][1]
-        for i in range(len(self.matrice)):
-            for j in range(len(self.matrice[0])):
-                if self.matrice[i][j] == 3:
-                    if randint(0,100) < prob_0:
-                        self.matrice[i][j] = 0
-                    if randint(0,100) < prob_1:
-                        self.matrice[i][j] = 1
+    def creer_des_trous(self):
+        while self.nb_trous > 0:  
+            x_trou, y_trou = randint(0, self.width), randint(0, self.width)
+            self.matrice[x_trou][y_trou] = 0
+            
+            self.algo.plan = self.matrice
+            self.algo.reset_distance_map()
+            self.algo.algo_main()
+            
+            if self.algo.way != []:
+                self.nb_trous -= 1
+            else:
+                self.matrice[x_trou][y_trou] = 1
                         
     def créer_file_csv(self):
         with open("maze.csv",'w',newline = '') as f:
@@ -73,30 +75,8 @@ class Matrice():
             writer.writerows(self.matrice)
            # print(self.chemin)
          
-    def update_matrice(self, i,j):
-        if self.matrice[j][i] !=0:
+if __name__ == "__main__":
+    mat = Matrice("facile")
             
-            row = self.debut[0]
-            col = self.debut[1]
-            if abs(i-self.debut[0])==1 and abs(j-self.debut[1])==1:
-                self.distance += math.sqrt(abs(self.matrice[col][row]))
-            else:
-                self.distance += abs(self.matrice[col][row])
-            self.matrice[j][i] *= -1
-            self.debut= (i,j)
-            self.créer_file_csv()
-            
-    def chemin_passe(self, point):
-        if point not in self.chemin:
-            self.chemin += point
-            self.créer_file_csv()
-            
-    def delete_item(self):
-        self.chemin.pop()
-        self.créer_file_csv()
-        
-    def restart(self):
-        self.chemin = []
-        self.créer_file_csv()
                 
 
